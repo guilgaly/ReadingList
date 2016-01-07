91f4eccbc98bd66ccbea3127ee7cb4050367a680
Guillaume Galy <guilgaly@gmail.com>
2016-01-08 00:53:42 +0100

    Added project Lombok for good measure! :)

--- src/main/java/guilgaly/readinglist/Book.java
+++ src/main/java/guilgaly/readinglist/Book.java
@@ -5,9 +5,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Entity
-public class Book {
+import lombok.Data;
 
+@Entity
+@Data
+public class Book {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private long id;
@@ -16,52 +18,4 @@ public class Book {
     private String title;
     private String author;
     private String description;
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(final long id) {
-        this.id = id;
-    }
-
-    public String getReader() {
-        return reader;
-    }
-
-    public void setReader(final String reader) {
-        this.reader = reader;
-    }
-
-    public String getIsbn() {
-        return isbn;
-    }
-
-    public void setIsbn(final String isbn) {
-        this.isbn = isbn;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(final String title) {
-        this.title = title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(final String author) {
-        this.author = author;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(final String description) {
-        this.description = description;
-    }
 }
